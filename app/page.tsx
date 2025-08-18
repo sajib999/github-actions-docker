c19058422d5d987a5d2e2fdc8acb4247c1b6ac3a
sajib999 <eshaq.feel@gmail.com>
2025-08-18 16:15:48 +0600

    Add h2 and p tag

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,15 @@ export default function Home() {
     <div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
       <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
         <h1 className='text-lime-700 text-5xl'>Github Actions With Docker</h1>
+        <h2 className='text-cyan-700 text-2xl'>The more the merrier</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+          doloremque ex doloribus eum, vero asperiores quo porro omnis facilis
+          id maxime quam ducimus deserunt impedit, quisquam fugiat reiciendis!
+          Ratione magnam eveniet tempora laboriosam accusamus totam deleniti,
+          iste reprehenderit velit? Nisi rem placeat error ipsam! Omnis velit
+          eos vitae asperiores est natus id, minus aliquid quam.
+        </p>
       </main>
     </div>
   );
