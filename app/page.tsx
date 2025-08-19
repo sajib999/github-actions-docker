269d53049af50a03e23b2f5977142669a8f84dd1
sajib999 <eshaq.feel@gmail.com>
2025-08-19 15:24:14 +0600

    GitHub Container Registry

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
       <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
         <h1 className='text-lime-700 text-5xl'>Github Actions With Docker</h1>
-        <h2 className='text-cyan-700 text-2xl'>The more the merrier</h2>
+        <h2 className='text-rose-700 text-2xl'>The more the merrier</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
           doloremque ex doloribus eum, vero asperiores quo porro omnis facilis
